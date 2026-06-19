1128f66db7917fa92b85f8d606a5275b23b06869
Zethas-ops <fauzizuhdi.h@gmail.com>
2026-06-19 14:16:33 +0700

    fix error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, ".", "");
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react(), tailwindcss()],
     define: {
